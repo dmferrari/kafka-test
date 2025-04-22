6dda98f13a22b923c1c26a80fb755063e084c88a
Martin Ferrari <dmferrari@gmail.com>
2025-04-22 09:51:57 -0300

    First working version

---
+++ consumer.rb
@@ -0,0 +1,7 @@
+require 'kafka'
+
+kafka = Kafka.new(["localhost:9092"])
+
+kafka.each_message(topic: "test-topic") do |message|
+  puts "Received: #{message.offset} - #{message.value}"
+end
---
+++ producer.rb
@@ -0,0 +1,8 @@
+require 'kafka'
+
+kafka = Kafka.new(["localhost:9092"])
+producer = kafka.producer
+
+producer.produce("Hello from ruby!", topic: "test-topic")
+producer.deliver_messages
+puts "Message sent!"
